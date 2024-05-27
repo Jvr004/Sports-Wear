1eca5eec4341654c4eab352cd6ab899a934c6f77
Jvr <jvr.t004@gmail.com>
2024-05-27 11:32:32 -0300

    Más contenido html y etilos css

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// header y footer dinamico. para todas las paginas. No funciono :(
+const header = document.querySelectorAll('header');
+const footer = document.querySelector('footer');
+  
+// header.innerHTML = '
+//   < a href = "index.html" > <i class="fa-solid fa-chevron-up fa-beat-fade"></i></ >
+      
+//       <div class="logo" id="logo">
+//         <a href="index.html">
+//           <img src="images/Logo.gif" width="100px" alt="Imagen logo"/>
+//         </a>
+//       </div>
+
+//       <!--Nav-->
+//   <nav class="navbar">
+//     <div class="nav-links" id="nav-links">
+//       <ul>
+//         <li><a href="index.html">Inicio</a></li>
+//         <li><a href="#">Productos</a></li>
+//         <li><a href="mayorista.html">Venta Mayorista</a></li>
+//         <li><a href="locales.html">Locales</a></li>
+//         <li><a href="guia-talles.html">Guia de Talles</a></li>
+//         <li><a href="login.html"><i class="fa-regular fa-user fa-xl"></i></a></li>
+//         <li><a href="#"><i class="fa-solid fa-bag-shopping fa-xl"></i></a></li>
+//       </ul>
+//     </div>
+//   </nav>
+// ';
